a4210be968a93132b42cc2a4f7eb5c8213e98efd
DESKTOP-GSB0K5T\Fleshgod4618 <kt165czop@gmail.com>
2017-08-15 21:17:32 +0900

    課題　ファイルから読み出しと表示を提出します

--- CampChallenge_java/015.標準クラス/task9.java
+++ CampChallenge_java/015.標準クラス/task9.java
@@ -1,85 +1,196 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+
+
+ 
+ * To change this license header, choose License Headers in Project Properties. 
+
+
+ 
+ * To change this template file, choose Tools | Templates 
+
+
+ 
+ * and open the template in the editor. 
+
+
+ 
  */
 
+import java.io.BufferedReader;
+
+import java.io.FileReader;
+
 import java.io.IOException;
+
 import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
+
 import javax.servlet.http.HttpServlet;
+
 import javax.servlet.http.HttpServletRequest;
+
 import javax.servlet.http.HttpServletResponse;
 
 /**
+ * *
  *
- * @author Fleshgod4618
+ *
+ *
+ *
+ *
+ *
+ * @author
+ *
+ *
  */
 public class task9 extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
+     * *
      *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     *
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> *
+     *
+     *
+     * methods. *
+     *
+     *
+     *
+     *
+     *
+     *
+     * @param request servlet request *
+     *
+     *
+     * @param response servlet response *
+     *
+     *
+     * @throws ServletException if a servlet-specific error occurs *
+     *
+     *
+     * @throws IOException if an I/O error occurs *
+     *
+     *
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet task9</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet task9 at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+
+        PrintWriter out = response.getWriter();
+
+        String file3 = "/Users/Fleshgod4618/Desktop/dr/file4.txt";
+
+        FileReader fr = new FileReader(file3);
+
+        BufferedReader br = new BufferedReader(fr);
+
+        String line;
+
+        int count = 0;
+
+        while ((line = br.readLine()) != null) {
+
+            out.println(count + "行目：" + line
+            );
+
         }
+
+        br.close();
+
+        fr.close();
+
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the  sign on the left to edit the code."> 
     /**
-     * Handles the HTTP <code>GET</code> method.
+     * *
      *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     *
+     * Handles the HTTP <code>GET</code> method. *
+     *
+     *
+     *
+     *
+     *
+     *
+     * @param request servlet request *
+     *
+     *
+     * @param response servlet response *
+     *
+     *
+     * @throws ServletException if a servlet-specific error occurs *
+     *
+     *
+     * @throws IOException if an I/O error occurs *
+     *
+     *
      */
     @Override
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         processRequest(request, response);
+
     }
 
     /**
-     * Handles the HTTP <code>POST</code> method.
+     * *
      *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     *
+     * Handles the HTTP <code>POST</code> method. *
+     *
+     *
+     *
+     *
+     *
+     *
+     * @param request servlet request *
+     *
+     *
+     * @param response servlet response *
+     *
+     *
+     * @throws ServletException if a servlet-specific error occurs *
+     *
+     *
+     * @throws IOException if an I/O error occurs *
+     *
+     *
      */
     @Override
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         processRequest(request, response);
+
     }
 
     /**
-     * Returns a short description of the servlet.
+     * *
      *
-     * @return a String containing servlet description
+     *
+     * Returns a short description of the servlet. *
+     *
+     *
+     *
+     *
+     *
+     *
+     * @return a String containing servlet description *
+     *
+     *
      */
     @Override
+
     public String getServletInfo() {
+
         return "Short description";
-    }// </editor-fold>
+
+    }// </editor-fold> 
 
 }
