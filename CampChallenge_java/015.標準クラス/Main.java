4aabb5dbe2df109b948df9593cc1c6ce77540b0b
DESKTOP-GSB0K5T\Fleshgod4618 <kt165czop@gmail.com>
2017-08-15 21:26:05 +0900

    課題　標準クラスを利用して処理を作成を提出します

---
+++ CampChallenge_java/015.標準クラス/Main.java
@@ -0,0 +1,64 @@
+/* 
+
+
+ 
+ * To change this license header, choose License Headers in Project Properties. 
+
+
+ 
+ * To change this template file, choose Tools | Templates 
+
+
+ 
+ * and open the template in the editor. 
+
+
+ 
+ */
+
+
+import java.util.*;
+
+//import java.util.Calendar; 
+import java.text.*;
+
+import java.io.*;
+
+/**  *
+ *
+ *
+ *
+ *
+ *
+ *
+ * @author 
+ *
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        tasks test = new tasks();
+
+        test.task1();
+
+        test.task2();
+
+        test.task3();
+
+        test.task4();
+
+        test.task5();
+
+        test.task6();
+
+        test.task7();
+
+        test.task8();
+
+        test.task9();
+
+    }
+
+}
