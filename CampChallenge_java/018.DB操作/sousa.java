6428b003d64781ace111cc6a20fc6c1aa574fc82
DESKTOP-GSB0K5T\Fleshgod4618 <kt165czop@gmail.com>
2017-09-04 15:46:28 +0900

    課題　特定のレコードの情報取得と表示を提出します

---
+++ CampChallenge_java/018.DB操作/sousa.java
@@ -0,0 +1,134 @@
+package Database;
+
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+
+import javax.servlet.http.HttpServlet;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
+
+/**
+ * *
+ *
+ *
+ *
+ *
+ *
+ *
+ * @author
+ *
+ *
+ */
+public class sousa extends HttpServlet {
+
+    /**
+     * *
+     *
+     *
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> *
+     *
+     *
+     * methods. *
+     *
+     *
+     *
+     *
+     *
+     *
+     * @param request servlet request *
+     *
+     *
+     * @param response servlet response *
+     *
+     *
+     * @throws ServletException if a servlet-specific error occurs *
+     *
+     *
+     * @throws IOException if an I/O error occurs *
+     *
+     *
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        Connection db_con = null;
+
+        PreparedStatement db_st = null;
+
+        ResultSet db_data = null;
+
+        try (PrintWriter out = response.getWriter()) {
+
+            /* TODO output your page here. You may use following sample code. */
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+        db_con = DriverManager.getConnection("jdbc:mysql://localhost:3306/DATA_db", "root", "");
+
+            if (db_con != null) {
+
+                out.print("接続成功しました。<br><br>");
+
+            }
+
+            //「a = b」は「aがbと等しい」という意味 
+            //「name = ?」にすると名前が「茂」の人以外は外れてしまうので、 
+            //「aはbを含む」という意味の「a LIKE '%b%'(aは「b」を含む。部分一致)」を使う 
+            db_st = db_con.prepareStatement("select*from profiles where name like ?");
+
+            db_st.setString(1, "%茂%");
+
+            db_data = db_st.executeQuery();
+
+            while (db_data.next()) {
+
+                out.print("ID：" + db_data.getInt("profilesID") + "<br>");
+
+                out.print("名前：" + db_data.getString("name") + "<br>");
+
+                out.print("電話番号：" + db_data.getString("tel") + "<br>");
+
+                out.print("年齢：" + db_data.getInt("age") + "<br>");
+
+                out.print("誕生日：" + db_data.getString("birthday")
+                        + "<br><br>");
+
+            }
+
+        } catch (SQLException e_sql) {
+
+            System.out.println("接続時にエラーが発生しました。");
+
+        } catch (Exception e) {
+
+            System.out.println("エラーが発生しました。");
+
+        } finally {
+
+            if (db_con != null) {
+
+                try {
+
+                    db_con.close();
+
+                } catch (Exception e_con) {
+
+                    System.out.println(e_con.getMessage());
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
